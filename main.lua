90178912e79b7c8b73a1ad29a64e4dd9e1a11396
Lobinux <lobinux@digilearnnials.com>
2021-09-28 12:15:51 -0300

    Draw snake ♥

--- main.lua
+++ main.lua
@@ -2,12 +2,12 @@ function love.load()
   --Object = require "classic"
   --require "circle"
   --circle1 = Circle(200,200,5,300)
-  
-  circle = {}
 
-  circle.x = 100
-  circle.y = 100
-  circle.radius = 50
+  maxBodyPieces = 10
+
+  listOfCircle = {}
+
+  createCircle(5)
 
   playerX = 0
   playerY = 0
@@ -16,30 +16,55 @@ end
 
 function love.update(dt)
   if love.keyboard.isDown("right") then
-      playerX = playerX+300*dt
+      playerX = 300
+      playerY = 0
     end
   
     if love.keyboard.isDown("left") then
-      playerX = playerX-300*dt
+      playerX = -300
+      playerY = 0
     end
     
     if love.keyboard.isDown("down") then
-      playerY = playerY+300*dt
+      playerX = 0
+      playerY = 300
     end
     
     if love.keyboard.isDown("up") then
-      playerY = playerY-300*dt
+      playerX = 0
+      playerY = -300
     end
   
-  if playerX >900 then
-    playerX = 0
+  for i = maxBodyPieces-1, 1, -1 do
+    listOfCircle[i].x = listOfCircle[i-1].x
+    listOfCircle[i].y = listOfCircle[i-1].y
   end
-  
-    
+
+  listOfCircle[0].x = (playerX * dt) + listOfCircle[0].x
+  listOfCircle[0].y = (playerY * dt) + listOfCircle[0].y
 end
 
 function love.draw()
-    love.graphics.rectangle("line", playerX, playerY, 50, 80)
-    love.graphics.circle("line", circle.x, circle.y, circle.radius, 12)
+  for i = 0, maxBodyPieces,1 do
+    love.graphics.circle("line", listOfCircle[i].x, listOfCircle[i].y, listOfCircle[i].radius, 18)
+  end
+    -- for index, value in ipairs(listOfCircle) do
+    --   love.graphics.circle("line", value.x, value.y, value.radius, 18)
+    -- end
+
+    
 end
 
+function createCircle(radius)
+  
+  for i = 0, maxBodyPieces,1 do
+
+    circle = {}
+    circle.x = 0
+    circle.y = 0
+    circle.radius = radius
+
+    listOfCircle[i] = circle;
+  end
+  
+end
