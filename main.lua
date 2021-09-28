7aef8c7f701967fa4c04f6b4de4982bc5f4b681e
HavensurgeSW <matias@karplus.com.ar>
2021-09-28 10:21:07 -0300

    Added arrow keys movement

--- main.lua
+++ main.lua
@@ -2,18 +2,36 @@
 
 function love.load()
   x = 0
+  y = 0
+  move = false
 end
 
 function love.update(dt)
-  x = x+300*dt
+  if love.keyboard.isDown("right") then
+      x = x+300*dt
+    end
+  
+    if love.keyboard.isDown("left") then
+      x = x-300*dt
+    end
+    
+    if love.keyboard.isDown("down") then
+      y = y+300*dt
+    end
+    
+    if love.keyboard.isDown("up") then
+      y = y-300*dt
+    end
+  
   if x >900 then
     x = 0
-    end
+  end
+  
     
 end
 
 function love.draw()
     love.graphics.print("Hello World!", 100, 100)
-    love.graphics.rectangle("line", x, 300, 50, 80)
+    love.graphics.rectangle("line", x, y, 50, 80)
 end
 
