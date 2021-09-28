552bf62c582eae5eb0cb81f79e10331392501708
HavensurgeSW <matias@karplus.com.ar>
2021-09-28 10:27:51 -0300

    Updated variable names

--- main.lua
+++ main.lua
@@ -1,37 +1,36 @@
 
 
 function love.load()
-  x = 0
-  y = 0
+  playerX = 0
+  playerY = 0
   move = false
 end
 
 function love.update(dt)
   if love.keyboard.isDown("right") then
-      x = x+300*dt
+      playerX = playerX+300*dt
     end
   
     if love.keyboard.isDown("left") then
-      x = x-300*dt
+      playerX = playerX-300*dt
     end
     
     if love.keyboard.isDown("down") then
-      y = y+300*dt
+      playerY = playerY+300*dt
     end
     
     if love.keyboard.isDown("up") then
-      y = y-300*dt
+      playerY = playerY-300*dt
     end
   
-  if x >900 then
-    x = 0
+  if playerX >900 then
+    playerX = 0
   end
   
     
 end
 
 function love.draw()
-    love.graphics.print("Hello World!", 100, 100)
-    love.graphics.rectangle("line", x, y, 50, 80)
+    love.graphics.rectangle("line", playerX, playerY, 50, 80)
 end
 
