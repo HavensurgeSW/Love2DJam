e76bbd300b0ead13cf505ac8ddd50ff84ada1272
HavensurgeSW <matias@karplus.com.ar>
2021-09-28 10:15:47 -0300

    Tested IF statements with Figure movement

--- main.lua
+++ main.lua
@@ -1,3 +1,19 @@
+
+
+function love.load()
+  x = 0
+end
+
+function love.update(dt)
+  x = x+300*dt
+  if x >900 then
+    x = 0
+    end
+    
+end
+
 function love.draw()
     love.graphics.print("Hello World!", 100, 100)
+    love.graphics.rectangle("line", x, 300, 50, 80)
 end
+
